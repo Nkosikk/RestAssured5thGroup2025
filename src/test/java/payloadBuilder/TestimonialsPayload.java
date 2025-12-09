ad8d2ded15ed82d6069527737c6762437fed151a
keishiablouws <kmblouws@gmail.com>
2025-12-09 18:30:02 +0200

    created the create testimonial and update testimonial method

--- src/test/java/payloadBuilder/TestimonialsPayload.java
+++ src/test/java/payloadBuilder/TestimonialsPayload.java
@@ -7,9 +7,29 @@ public class TestimonialsPayload {
     public JSONObject loginUserPayload(String email, String password) {
 
         JSONObject loginUser = new JSONObject();
-        loginUser.put("email",email);
-        loginUser.put("password",password);
+        loginUser.put("email", email);
+        loginUser.put("password", password);
 
         return loginUser;
     }
+
+    public JSONObject CreateTestimonialPayload(String title, String content, Integer rating, Boolean isPublic) {
+        JSONObject CreateTestimonial = new JSONObject();
+        CreateTestimonial.put("title", title);
+        CreateTestimonial.put("content", content);
+        CreateTestimonial.put("rating", rating);
+        CreateTestimonial.containsValue(isPublic);
+
+        return CreateTestimonial;
+    }
+
+    public JSONObject UpdateTestimonialPayload(String content, Integer rating) {
+        JSONObject UpdateTestimonial = new JSONObject();
+        UpdateTestimonial.put("content", content);
+        UpdateTestimonial.put("rating", rating);
+
+        return UpdateTestimonial;
+    }
+
+
 }
